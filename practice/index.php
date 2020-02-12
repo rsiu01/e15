72f3f662ce6e166d0a4a5a55a07bed3a91a020da
rsiu01 <rsiu01@gmail.com>
2020-02-11 19:52:20 -0500

    Edited index file and added an image

--- practice/index.php
+++ practice/index.php
@@ -1,10 +1,15 @@
 <!doctype html>
+
 <html lang='en'>
+
 <head>
 	<title>Practice Application</title>
 	<meta charset='utf-8'>
 </head>
+
 <body>
 	<h1>Practice Application</h1>
+	<img src='/images/crinkles.JPG' alt='Crinkles'>
 </body>
+
 </html>
